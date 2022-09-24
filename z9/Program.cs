3e86e6c4b109f5ed029f74f051c1d2ecd3f0a62f
evgenii <evgenii.lyabov@mail.ru>
2022-09-24 12:27:46 +0300

    add z9

---
+++ z9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Прогр. выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+
+
+int randnum = new Random().Next(10, 100);
+int a1 = randnum / 10;
+int a2 = randnum % 10;
+int max = a1;
+if(a2 > a1)
+{
+    max= a2;
+}
+Console.WriteLine($"Рандомное число {randnum} ");
+Console.WriteLine($"Максимальная цифра чила {max} ");
